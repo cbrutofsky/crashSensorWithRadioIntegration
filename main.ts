9b31878a2984fd1892d5813578e666ca252ab459
cbrutofsky <cbrutofsky@users.noreply.github.com>
2019-11-09 13:26:10 -0500

    Initial Commit

--- main.ts
+++ main.ts
@@ -1,7 +1,34 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+OLED.init(128, 95);
+tinkercademy.crashSensorSetup(DigitalPin.P8)
+function sensorControl() {
+    let display = "Hello, \
+              I'm a Crash Sensor!!!";
+
+    if (tinkercademy.crashSensor()) {
+        display = "Crash Sensor Impact!!";
+    }
+    return display;
+}
+
+function radioSignal(screen_message: string): void {
+    if (screen_message == "Crash Sensor Impact!!") {
+        radio.sendString("HELP!!!");
+    }
+}
+
+function recieveRadioSignal(message_recieved: string): boolean {
+    let ret = false;
+    if (message_recieved == null) {
+
+    }
+    return ret;
+}
+
+basic.forever(function () {
+    OLED.clear();
+    let screen_message = sensorControl();
+    radioSignal(screen_message)
+    OLED.writeStringNewLine(screen_message);
+    basic.pause(2000);
+    OLED.clear();
+})
