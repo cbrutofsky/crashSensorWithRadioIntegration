763080398d699e7c18e5d36b46af83b6c22c093c
cbrutofsky <cbrutofsky@users.noreply.github.com>
2019-11-12 22:37:12 -0500

    Final commit (with comments)

--- main.ts
+++ main.ts
@@ -9,9 +9,10 @@ tinkercademy.crashSensorSetup(DigitalPin.P8);
 
 /**
  * Function that controls the message that the OLED is displaying.
+ * Parameters: message (string)
  * Default is set to the START_MESSAGE constant.
  * If the message changes the screen is cleared before displaying the message.
- * Parameters: String message
+ * void function 
  */
 function screenDisplay(default_display = START_DISPLAY, current_display: string): void {
     if (current_display == default_display) {
@@ -29,7 +30,9 @@ function screenDisplay(default_display = START_DISPLAY, current_display: string)
 }
 
 /**
- * 
+ * Function that controls the crash sensor
+ * returns true: if the crash sensor is hit the display changes showing that there was some impact and the function
+ * default: return false
  */
 function sensorControl(): boolean {
     let ret = false
@@ -40,6 +43,12 @@ function sensorControl(): boolean {
     return ret
 }
 
+/**
+ * Function that sends radio signal if the crash sensor is invoked
+ * parameter: signal (boolean)
+ * return true: if a message is successfully sent
+ * default: return false
+ */
 function sendRadioSignal(signal: boolean): boolean {
     let ret = false;
     if (signal) {
@@ -51,6 +60,12 @@ function sendRadioSignal(signal: boolean): boolean {
     return ret;
 }
 
+/**
+ * Function that listens for a radio signal from another microbit on the same channel
+ * parametrs: the current OLED display status (string)
+ * return true: if a message is successfully recieved
+ * default: return false
+ */
 function recieveRadioSignal(current_status: string): boolean {
     let ret = false;
     radio.onReceivedString(function (receivedString: string) {
@@ -68,21 +83,13 @@ function recieveRadioSignal(current_status: string): boolean {
 basic.forever(function () {
     // initialize the sensorControl
     let sensor_control = sensorControl();
-    //let message_recieved = false;
+
+    // initialize the radio recieve
     let message_recieved = recieveRadioSignal(CRASH_DISPLAY)
 
     if (message_recieved == false && sensor_control == true) {
         sendRadioSignal(true);
     }
-    // if (sensor_control == true) {
-    //     if (message_recieved == false) {
-    //         basic.pause(1000)
-    //         sendRadioSignal(false)
-    //     }
-    //     else {
-    //         sensor_control = false;
-    //     }
-    // }
 
     input.onButtonPressed(Button.AB, function () {
         screenDisplay(null, START_DISPLAY);
